3bf3f4aec168cc246ad5015287ad895fe1f21faf
xl <eveg@qq.com>
2023-07-04 14:30:50 +0800

    fix: pnpm lock 更新

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home,
+  // },
+  // {
+  //   path: "/developer",
+  //   name: "Developer",
+  //   component: () => import(/* webpackChunkName: "articles" */ "../views/portal/developer-center.vue")
+  // },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
