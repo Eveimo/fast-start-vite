54ba007b148d605da4d28d7ca54ef0ec67649a83
xl <eveg@qq.com>
2023-07-28 10:33:42 +0800

    feat: 新增404页面以及首页

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <HelloWorld msg="element-plus" />
+  <RouterView></RouterView>
 </template>
 
 <style scoped lang="less">
